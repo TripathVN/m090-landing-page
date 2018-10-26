06e7505c556071ef84b70643415b8670dc94daef
tudx <tudx@tripath.vn>
2018-10-26 10:32:03 +0700

    Fix bug ScrollMagic Parallax on Maps

--- src/js/scrollmagic.js
+++ src/js/scrollmagic.js
@@ -10,9 +10,10 @@ SM.parallax = function () {
 
     let tween = new TimelineMax()
         .add([
-            TweenMax.fromTo("#o-parallax figure", 1, {y: 35}, {y: -135, ease: Linear.easeNone}),
-            TweenMax.fromTo("#o-parallax .o-map__summary--right", 1, {y: 115}, {y: -285, ease: Linear.easeNone}),
-            TweenMax.fromTo("#o-parallax .o-map__summary--testimonial .summary", 1, {y: 75}, {y: -195, ease: Linear.easeNone})
+            TweenMax.fromTo("#o-map .o-map__left .o-map__summary figure", 1, {y: 35}, {y: -135, ease: Linear.easeNone}),
+            TweenMax.fromTo("#o-map .o-map__left .o-map__summary--right", 1, {y: 115}, {y: -285, ease: Linear.easeNone}),
+            TweenMax.fromTo("#o-map .o-map__left .o-map__summary--testimonial .summary", 1, {y: 75}, {y: -195, ease: Linear.easeNone}),
+            TweenMax.fromTo("#o-map .o-map__right", 1, {y: 55}, {y: -75, ease: Linear.easeNone})
         ]);
 
     let controller = new ScrollMagic.Controller({vertical: true});
