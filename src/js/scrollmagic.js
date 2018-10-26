14af7add7ca18f0a3e33a8307541084cbbbe2783
tudx <tudx@tripath.vn>
2018-10-26 11:08:09 +0700

    Fix some minor bugs

--- src/js/scrollmagic.js
+++ src/js/scrollmagic.js
@@ -13,7 +13,7 @@ SM.parallax = function () {
             TweenMax.fromTo("#o-map .o-map__left .o-map__summary figure", 1, {y: 35}, {y: -135, ease: Linear.easeNone}),
             TweenMax.fromTo("#o-map .o-map__left .o-map__summary--right", 1, {y: 115}, {y: -285, ease: Linear.easeNone}),
             TweenMax.fromTo("#o-map .o-map__left .o-map__summary--testimonial .summary", 1, {y: 75}, {y: -195, ease: Linear.easeNone}),
-            TweenMax.fromTo("#o-map .o-map__right", 1, {y: 55}, {y: -75, ease: Linear.easeNone})
+            TweenMax.fromTo("#o-map .o-map__right", 1, {y: 115}, {y: -95, ease: Linear.easeNone})
         ]);
 
     let controller = new ScrollMagic.Controller({vertical: true});
